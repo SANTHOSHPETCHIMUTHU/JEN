58d6bfe160dce7b1311788d4c9cea9ca31f39892
SANTHOSH KUMAR PETCHIMUTHU <149028859+SANTHOSHPETCHIMUTHU@users.noreply.github.com>
2025-05-28 08:17:58 +0530

    Create Apptest.java

---
+++ src/org/example/Apptest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testGreet() {
+        App app = new App();
+        assertEquals("Hello CI/CD!", app.greet());
+    }
+}
